5a861b1bfe24ef3bd53e422deadb81395cfde3ef
Azein Pwr <azeinpwr@gmail.com>
2016-10-06 17:46:43 +0600

    App init w/o hot reload and postcss

---
+++ app/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import styles from './index.css';
+
+
+
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
+
+
+const store = createStore(
+  applyMiddleware(logger, thunk)
+  
+) 
+
+class TestApp extends React.Component{
+  render(){
+    return(
+      <h2  className={styles.header}>Render Test</h2>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+ <Provider store={store}>
+   
+   <TestApp />
+
+  </Provider>
+  ,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+
+var webpack = require('webpack');
+
+var CommonsPlugin = new require("webpack/lib/optimize/CommonsChunkPlugin");
+var devFlagPlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+});
+
+var StyleLintPlugin = require('stylelint-webpack-plugin');
+
+var path = require('path');
+
+var config = {
+
+  context: path.resolve(__dirname + '/app'),
+  entry: {
+    app    : "./index.js",
+    vendor : ['react', 'react-dom', 'redux', 'react-redux', 'redux-thunk']
+  },
+
+  plugins: [
+        new CommonsPlugin({
+            name: 'vendor',
+            filename: 'vendor.js'
+        }),
+
+        new StyleLintPlugin({
+          configFile: 'stylelintrc.json',
+          context: path.resolve(__dirname + '/app'),
+          files: '**/*.css',
+          failOnError: false
+    })
+    ],
+
+  output: {
+    filename: "[name].js",
+    path: "./dist",
+  },
+
+  watch: true,
+
+  module: {
+  
+      preLoaders: [
+      {
+        test: /\.js?$/,
+        loaders: ['eslint-loader']
+      }
+    ],
+    
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel",
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    }, 
+    {
+      test: /\.css$/, // Only .css files
+      exclude: /.*\.woff$/,
+
+      loader: 'style!css' // Run both loaders
+    },
+
+    { test: /\.(png|jpg)$/, loader: 'file-loader' },
+    
+    {
+      test: /\.html$/,
+
+      loader: "file?name=[name].[ext]",
+    },
+    ],
+  },
+
+
+
+}
+
+module.exports = config;
